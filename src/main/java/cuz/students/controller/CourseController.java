c287244636e7824f443fc428ded973bbb404f68d
1328954760 <1328954760@qq.com>
2023-06-29 10:32:44 +0800

    initial commit

--- src/main/java/cuz/students/controller/CourseController.java
+++ src/main/java/cuz/students/controller/CourseController.java
@@ -4,11 +4,13 @@ import com.google.gson.Gson;
 import cuz.students.mapper.CourseMapper;
 import cuz.students.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 public class CourseController {
     @Autowired
--- src/main/java/cuz/students/controller/StudentController.java
+++ src/main/java/cuz/students/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import com.google.gson.Gson;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 public class StudentController {
     @Autowired
